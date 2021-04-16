ba8ddecce45e9d1c8685fb1886acb7fd08f91898
Anh Nguyen <anhnd@bravestars.com>
2021-04-16 11:18:40 +0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,17 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-74690900-6", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-remark-highlight-code`,
     },
     {
